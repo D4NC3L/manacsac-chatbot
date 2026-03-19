fd10bf1b50dea8f9d34fa23b8a2b903a046115d7
Cel. <128194240+D4NC3L@users.noreply.github.com>
2026-03-19 17:57:27 +0800

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
   }
 
   const { message } = req.body;
-  const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+  const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY=AIzaSyCssO34Ya48BXdiVF2hDo2mkPSLO7IaaE8);
   const model = genAI.getGenerativeModel({ model: "gemini-1.5" });
 
   // KNOWLEDGE BASE (Dito ilalagay ang info ng school)
